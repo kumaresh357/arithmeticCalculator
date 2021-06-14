fc97386d7f6d242c79e8500fbe2cc6bef83a3599
Kumaresh Dey <dey.kumaresh357@gmail.com>
2021-06-14 21:10:16 +0530

    final commit arithmeticCalculator

--- ArithmeticCalculator.java
+++ ArithmeticCalculator.java
@@ -3,33 +3,22 @@ import java.util.Scanner;
 
 public class ArithmeticCalculator {
 	public static void main(String[] args) {
-		Scanner marks = new Scanner(System.in);
-		System.out.println("Enter your marks1");
-		float a = marks.nextFloat();
-		System.out.println("Enter your marks2");
-		float b = marks.nextFloat();
-		System.out.println("Enter your marks3");
-		float c = marks.nextFloat();
-		System.out.println("Enter your marks4");
-		float d = marks.nextFloat();
-		System.out.println("Enter your marks5");
-		float e = marks.nextFloat();
-		System.out.println("sum of marks1 and marks2");
+		Scanner num = new Scanner(System.in);
+		System.out.println("Enter number1");
+		float a = num.nextFloat();
+		System.out.println("Enter number2");
+		float b = num.nextFloat();
+		System.out.println("sum of number1 and number2");
 		float sum = a+b;
 		System.out.println(sum);
-		System.out.println("diffrence between marks2 and marks3");
-		float subtract = b-c;
+		System.out.println("diffrence between number1 and number2");
+		float subtract = a-b;
 		System.out.println(subtract);
-		System.out.println("product of marks3 and marks4");
-		float product = c*d;
+		System.out.println("product of number1 and number2");
+		float product = a*b;
 		System.out.println(product);
-		System.out.println("division of marks4 and marks5");
-		float division = d/e;
+		System.out.println("division of number1 and number2");
+		float division = a/b;
 		System.out.println(division);
-		System.out.println("total percentage");
-		float percentage = (a+b+c+d+e)*100/500;
-		System.out.println(percentage + "%");
 	}
-	
-
 }
